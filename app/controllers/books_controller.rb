59bcc61ef7adcfba1b5a443985ac9882f7303f5f
Josh Parker <joshBBparker@gmail.com>
2022-04-04 18:01:01 -0600

    ci setup part 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
   def show
     @book = Book.find(params[:id])
     @reviews = @book.reviews
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class ReviewsController < ApplicationController
   def create
     @review = Review.new(post_params)
-    if @review.save
-      redirect_to root_path
-    end
+    redirect_to root_path if @review.save
   end
+
   private
+
   def post_params
     params.require(:review).permit(:author, :comment, :book_id)
   end
